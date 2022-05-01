8eee7db3f46a9c4d17252b55d5c3ea7bf94cbea6
Hoang Quyen <hoangquyen18012001@gmail.com>
2022-05-01 22:26:07 +0700

    Code Todo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Welcome to Quyen's world</p>
-			</header>
+			<header className='App-header'></header>
 		</div>
 	);
 }
---
+++ src/Features/Todo/Components/TodoList/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    
+};
+TodoList.defaultProps = {
+    todoList : [],
+}
+
+function TodoList(props) {
+    const {} = props;
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TodoList;
