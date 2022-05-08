1fcd35fec8e371d7c3c385e64c56d11f4066e966
Hoang Quyen <hoangquyen18012001@gmail.com>
2022-05-08 20:56:18 +0700

    Null

---
+++ src/API/CategoryAPI.js
@@ -0,0 +1,26 @@
+import axiosClient from './AxiosClient';
+
+const CategoryApi = {
+    getAll(params) {
+        const url = '/categories';
+        return axiosClient.get(url, { params });
+    },
+    get(id) {
+        const url = `/categories/${id}`;
+        return axiosClient.get(url);
+    },
+    add(data) {
+        const url = '/categories';
+        return axiosClient.post(url, data);
+    },
+    update(data) {
+        const url = `/categories/${data.id}`;
+        return axiosClient.post(url, data);
+    },
+    remove(id) {
+        const url = `/categories/${id}`;
+        return axiosClient.delete(url);
+    },
+};
+
+export default CategoryApi;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import ProductApi from './API/ProductAPI';
 import './App.css';
 import ColorBox from './Components';
 import AlbumFeature from './Features/Album';
 import TodoFeature from './Features/Todo';
+
 function App() {
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const params = {
+                _limit: 10,
+            };
+            const productsList = await ProductApi.getAll(params);
+            console.log(productsList);
+        };
+        fetchProduct();
+    }, []);
     return (
         <div className="App">
             Home Page
